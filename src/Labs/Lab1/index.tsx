874b6d66d6d28180cc0aca3616495aa714dfeef2
Sid <siddharthr3535@gmail.com>
2025-01-22 22:21:57 -0500

    adding some details for lab1

--- src/Labs/Lab1/index.tsx
+++ src/Labs/Lab1/index.tsx
@@ -1,9 +1,12 @@
 export default function Lab1() {
-    return (
-      <div id="wd-lab1">
-
+  return (
+    <div id="wd-lab1">
+      <h2>Lab 1</h2>
+      <h3>Default Lab page</h3>
+      <div id="wd-h-tag">
+        <h4>Default Heading</h4>
+        This is the description of lab 1.
       </div>
-    );
-  }
-  
-  
+      {/* do the next exercise here */}
+    </div>
+);}
