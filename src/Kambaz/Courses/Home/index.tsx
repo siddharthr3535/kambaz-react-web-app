db8a1f9a9c452583b0e3b16c1e0fd243a8882de4
Sid <siddharthr3535@gmail.com>
2025-02-04 11:02:18 -0500

    Fixing the course status issue

--- src/Kambaz/Courses/Home/index.tsx
+++ src/Kambaz/Courses/Home/index.tsx
@@ -1,18 +1,14 @@
-import { Container } from "react-bootstrap";
 import Modules from "../Modules";
 import CourseStatus from "./Status";
 export default function Home() {
   return (
-    // <div className="d-flex" id="wd-home">
-    <Container>
+    <div className="d-flex" id="wd-home">
       <div className="flex-fill me-3">
         <Modules />
       </div>
-
       <div className="d-none d-xl-block">
         <CourseStatus />
       </div>
-    </Container>
-    // </div>
+    </div>
   );
 }
