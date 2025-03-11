dc13ab6c06bf637b5a31978eab0c6b7fafbf103c
Sid <siddharthr3535@gmail.com>
2025-03-11 18:14:04 -0400

    adding lab4 to the index

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -1,58 +1,74 @@
-// import { Link } from "react-router-dom";
-import Nav from "react-bootstrap/Nav";
-import { useLocation } from "react-router";
 import { Link } from "react-router-dom";
+import { useLocation } from "react-router";
 
 export default function TOC() {
   const { pathname } = useLocation();
-
   return (
-    <Nav variant="pills" id="wd-toc">
-      <Nav.Item>
-        {" "}
-        <Nav.Link
-          as={Link}
-          to="/Labs/Lab1"
+    <ul className="nav nav-pills">
+      <li className="nav-item">
+        <a id="wd-a" href="#/Labs" className="nav-link">
+          Labs
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
           id="wd-a1"
-          active={pathname.includes("Lab1")}
+          href="#/Labs/Lab1"
+          className={`nav-link ${pathname.includes("Lab1") ? "active" : ""}`}
         >
-          {" "}
-          Lab 1{" "}
-        </Nav.Link>{" "}
-      </Nav.Item>
-      <Nav.Item>
-        {" "}
-        <Nav.Link
-          as={Link}
-          to="/Labs/Lab2"
+          Lab 1
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
           id="wd-a2"
-          active={pathname.includes("Lab2")}
+          href="#/Labs/Lab2"
+          className={`nav-link ${pathname.includes("Lab2") ? "active" : ""}`}
         >
-          {" "}
-          Lab 2{" "}
-        </Nav.Link>{" "}
-      </Nav.Item>
-      <Nav.Item>
-        {" "}
-        <Nav.Link
-          as={Link}
-          to="/Labs/Lab3"
+          Lab 2
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
           id="wd-a3"
-          active={pathname.includes("Lab3")}
+          href="#/Labs/Lab3"
+          className={`nav-link ${pathname.includes("Lab3") ? "active" : ""}`}
         >
-          {" "}
-          Lab 3{" "}
-        </Nav.Link>{" "}
-      </Nav.Item>
-
-      <Nav.Item>
-        <Nav.Link href="#/Kambaz">Kambaz</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link id="wd-github" href="https://github.com/siddharthr3535">
+          Lab 3
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          id="wd-a3"
+          href="#/Labs/Lab4"
+          className={`nav-link ${pathname.includes("Lab4") ? "active" : ""}`}
+        >
+          Lab 4
+        </a>
+      </li>
+      <li className="nav-item">
+        <a id="wd-k" href="#/Kanbas" className="nav-link">
+          Kanbas
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          id="wd-github-source-code"
+          href="https://github.com/siddharthr3535/kambaz-react-web-app"
+          className="nav-link"
+        >
+          Github Source Code
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          id="wd-github"
+          href="https://github.com/siddharthr3535"
+          className="nav-link"
+        >
           My GitHub
-        </Nav.Link>
-      </Nav.Item>
-    </Nav>
+        </a>
+      </li>
+    </ul>
   );
 }
--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,28 +1,28 @@
-import Lab1 from "./Lab1";
 import { Route, Routes, Navigate } from "react-router";
-import TOC from "./TOC";
+import store from "./store";
+import { Provider } from "react-redux";
+import Lab1 from "./Lab1";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
-import { Container } from "react-bootstrap";
+import TOC from "./TOC";
+import Lab4 from "./Lab4";
 export default function Labs() {
   return (
-    <Container>
-      <h2>Siddharth Ramachandran</h2>
-      Siddharth's Github:
-      <a
-        id="wd-github"
-        href="https://github.com/siddharthr3535/kambaz-react-web-app"
-      >
-        Siddharth's Github
-      </a>
-      <h1>Labs</h1>
-      <TOC />
-      <Routes>
-        <Route path="/" element={<Navigate to="Lab1" />} />
-        <Route path="Lab1" element={<Lab1 />} />
-        <Route path="Lab2/*" element={<Lab2 />} />
-        <Route path="Lab3/*" element={<Lab3 />} />
-      </Routes>
-    </Container>
+    <Provider store={store}>
+      <div id="wd=-labs">
+        <h1>Labs</h1>
+        <h2>Vicky Daniel Amalan</h2>
+        <h3>Section: 02</h3>
+        <hr></hr>
+        <TOC />
+        <Routes>
+          <Route path="/" element={<Navigate to="Lab1" />} />
+          <Route path="Lab1" element={<Lab1 />} />
+          <Route path="Lab2" element={<Lab2 />} />
+          <Route path="Lab3/*" element={<Lab3 />} />
+          <Route path="Lab4/*" element={<Lab4 />} />
+        </Routes>
+      </div>
+    </Provider>
   );
 }
