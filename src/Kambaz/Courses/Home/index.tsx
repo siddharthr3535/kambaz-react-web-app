73a38e18cdf641867859535896a6061a20cc438f
Sid <siddharthr3535@gmail.com>
2025-03-13 18:53:56 -0400

    removing container to bring course status on the right

--- src/Kambaz/Courses/Home/index.tsx
+++ src/Kambaz/Courses/Home/index.tsx
@@ -1,17 +1,14 @@
-import { Container } from "react-bootstrap";
 import Modules from "../Modules";
 import CourseStatus from "./Status";
 export default function Home() {
   return (
-    <Container>
-      {/* <div className="d-flex" id="wd-home"> */}
+    <div className="d-flex" id="wd-home">
       <div className="flex-fill me-3">
         <Modules />
       </div>
       <div className="d-none d-xl-block">
         <CourseStatus />
       </div>
-      {/* </div> */}
-    </Container>
+    </div>
   );
 }
