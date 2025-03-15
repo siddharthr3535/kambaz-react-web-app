00ceed3f083a18049510b574af934d48bcee8860
Sid <siddharthr3535@gmail.com>
2025-03-15 17:10:50 -0400

    fixing the course home page for both faculty and students to be the same

--- src/Kambaz/Courses/Home/index.tsx
+++ src/Kambaz/Courses/Home/index.tsx
@@ -1,14 +1,18 @@
+import { Container, Row, Col } from "react-bootstrap";
 import Modules from "../Modules";
 import CourseStatus from "./Status";
+
 export default function Home() {
   return (
-    <div className="d-flex" id="wd-home">
-      <div className="flex-fill me-3">
-        <Modules />
-      </div>
-      <div className="d-none d-xl-block">
-        <CourseStatus />
-      </div>
-    </div>
+    <Container id="wd-home" className="mt-4">
+      <Row>
+        <Col md={8}>
+          <Modules />
+        </Col>
+        <Col md={4} className="d-none d-xl-block">
+          <CourseStatus />
+        </Col>
+      </Row>
+    </Container>
   );
 }
