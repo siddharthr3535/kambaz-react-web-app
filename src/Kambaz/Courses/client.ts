2a524a7b1c92e2d6c8a91edb70aaf15ebfc6eda2
Sid <siddharthr3535@gmail.com>
2025-04-18 13:04:19 -0400

    axios with creds update

--- src/Kambaz/Courses/client.ts
+++ src/Kambaz/Courses/client.ts
@@ -38,6 +38,8 @@ export const createCourse = async (course: any) => {
   return data;
 };
 export const findUsersForCourse = async (courseId: string) => {
-  const response = await axios.get(`${COURSES_API}/${courseId}/users`);
+  const response = await axiosWithCredentials.get(
+    `${COURSES_API}/${courseId}/users`
+  );
   return response.data;
 };
